33548e9488b2f4a0b36351858eb05d50ee837d50
git@github.com:Royx44/prework-study-guide.git <rh863684@gmail.com>
2023-10-02 13:07:51 -0500

    added code to editor.js

--- 02-Challenge/Develop/client/src/js/editor.js
+++ 02-Challenge/Develop/client/src/js/editor.js
@@ -2,13 +2,15 @@
 import { getDb, putDb } from './database';
 import { header } from './header';
 
-export default class {
+export default class Editor {
   constructor() {
     const localData = localStorage.getItem('content');
 
-    // check if CodeMirror is loaded
+    // Check if CodeMirror is loaded
     if (typeof CodeMirror === 'undefined') {
-      throw new Error('CodeMirror is not loaded');
+      console.warn('CodeMirror is not loaded. Displaying a fallback editor or message.');
+      // You can display a fallback editor or a message to the user here
+      return;
     }
 
     this.editor = CodeMirror(document.querySelector('#main'), {
@@ -22,21 +24,39 @@ export default class {
       tabSize: 2,
     });
 
-    // When the editor is ready, set the value to whatever is stored in indexeddb.
-    // Fall back to localStorage if nothing is stored in indexeddb, and if neither is available, set the value to header.
-    getDb().then((data) => {
-      console.info('Loaded data from IndexedDB, injecting into editor');
-      this.editor.setValue(data || localData || header);
-    });
-
+    // Load data from IndexedDB and set it in the editor
+    this.loadData();
+    
     this.editor.on('change', () => {
+      // Update the local storage when the editor content changes
       localStorage.setItem('content', this.editor.getValue());
     });
 
-    // Save the content of the editor when the editor itself is loses focus
-    this.editor.on('blur', () => {
+    // Save the content to IndexedDB when the editor loses focus
+    this.editor.on('blur', async () => {
       console.log('The editor has lost focus');
-      putDb(localStorage.getItem('content'));
+      await this.saveData(this.editor.getValue());
     });
+  }
+
+  async loadData() {
+    try {
+      const data = await getDb();
+      console.info('Loaded data from IndexedDB, injecting into editor');
+      this.editor.setValue(data || localData || header);
+    } catch (error) {
+      console.error('Error loading data from IndexedDB:', error);
+      // Handle the error, possibly by displaying a message to the user
+    }
+  }
+
+  async saveData(content) {
+    try {
+      await putDb(content);
+      console.log('Data saved to IndexedDB');
+    } catch (error) {
+      console.error('Error saving data to IndexedDB:', error);
+      // Handle the error, possibly by displaying a message to the user
+    }
   }
 }
