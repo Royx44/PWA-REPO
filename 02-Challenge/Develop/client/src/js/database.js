bda59c376fe85004fd9598c6d93e6485710cf484
git@github.com:Royx44/prework-study-guide.git <rh863684@gmail.com>
2023-10-02 13:20:13 -0500

    added code to database.js

--- 02-Challenge/Develop/client/src/js/database.js
+++ 02-Challenge/Develop/client/src/js/database.js
@@ -13,9 +13,39 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const db = await initdb();
+  const tx = db.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  
+  try {
+    await store.add({ content });
+    console.log('Data added to IndexedDB');
+  } catch (error) {
+    console.error('Error adding data to IndexedDB:', error);
+  } finally {
+    tx.done;
+  }
+};
+
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const db = await initdb();
+  const tx = db.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  
+  try {
+    const data = await store.getAll();
+    console.log('Data retrieved from IndexedDB');
+    return data;
+  } catch (error) {
+    console.error('Error retrieving data from IndexedDB:', error);
+    return null;
+  } finally {
+    tx.done;
+  }
+};
+
 
 initdb();
